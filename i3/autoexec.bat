51de40d3564b64ac88157a994ce800895ef2aedc
Simon McFarlane <simon@piston.pw>
2015-10-08 19:02:05 -0700

    Start pulseaudio if on Linux

--- i3/autoexec.bat
+++ i3/autoexec.bat
@@ -3,3 +3,7 @@ xscreensaver -no-splash &
 feh --bg-fill ~/Downloads/1430600418538.jpg &
 ibus-daemon --xim &
 redshift -l 45.5236111:-122.675 -t 6500:4900 -r
+
+case $( uname -s ) in
+Linux) pulseaudio --start;;
+esac
