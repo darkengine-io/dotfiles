5a04fff88ab5eea866c1a96e7006adf53bbbde62
Simon McFarlane <simon@piston.pw>
2015-10-10 13:03:31 -0700

    Improve i3 autoexec formatting

--- i3/autoexec.bat
+++ i3/autoexec.bat
@@ -5,5 +5,8 @@ ibus-daemon --xim &
 redshift -l 45.5236111:-122.675 -t 6500:4900 -r &
 
 case $( uname -s ) in
-Linux) pulseaudio --start && /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &;;
+Linux)
+pulseaudio --start &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+;;
 esac
