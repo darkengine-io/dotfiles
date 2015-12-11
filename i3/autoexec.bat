e65293c60de3bcb84151ed04a296d7cde86d68ae
Simon McFarlane <simon@piston.pw>
2015-12-10 19:54:58 -0800

    Load wallpaper from ~/wallpaper.jpg

--- i3/autoexec.bat
+++ i3/autoexec.bat
@@ -1,6 +1,6 @@
 #!/bin/sh
 xscreensaver -no-splash &
-feh --bg-fill ~/Downloads/1430600418538.jpg &
+feh --bg-fill ~/wallpaper.jpg &
 ibus-daemon --xim &
 redshift -l 45.5236111:-122.675 -t 6500:4900 &
 
