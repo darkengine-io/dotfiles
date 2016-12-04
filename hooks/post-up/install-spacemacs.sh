7e166d3a24061c5c884d2da6d40f02dce1656f61
Simon McFarlane <sm@desu.ne.jp>
2016-12-04 13:53:04 -0800

    Update spacemacs as post-up hook

--- hooks/post-up/install-spacemacs.sh
+++ hooks/post-up/install-spacemacs.sh
@@ -3,5 +3,7 @@ if [ ! -d .emacs.d/ ]; then
     echo "==> Downloading Spacemacs..."
     git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 else
-    echo "==> .emacs.d/ already present"
+    echo "==> Updating Spacemacs..."
+    cd .emacs.d/
+    git pull
 fi
