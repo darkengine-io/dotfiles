63483ccd325893a0981be407ff9462728a2705b5
Simon McFarlane <sm@desu.ne.jp>
2017-07-18 12:23:09 -0700

    Remove spacemacs

--- hooks/post-up/install-spacemacs.sh
+++
@@ -1,9 +0,0 @@
-cd ~
-if [ ! -d .emacs.d/ ]; then
-    echo "==> Downloading Spacemacs..."
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-else
-    echo "==> Updating Spacemacs..."
-    cd .emacs.d/
-    git pull
-fi
